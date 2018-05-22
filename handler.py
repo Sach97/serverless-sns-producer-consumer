58068ffa434f3960e5cf4b713130a1bb37f1a8df
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2018-05-22 10:49:07 +0200

    blog implementation works

--- handler.py
+++ handler.py
@@ -1,24 +1,33 @@
+import boto3
 import json
+import datetime
+from datetime import timezone
+
+def producer(event, context):
+    sns = boto3.client('sns')
+
+    context_parts = context.invoked_function_arn.split(':')
+    topic_name = "my-topic-name"
+    topic_arn = "arn:aws:sns:{region}:{account_id}:{topic}".format(
+        region=context_parts[3], account_id=context_parts[4], topic=topic_name)
+
+    now = datetime.datetime.now(timezone.utc)
+    start_date = (now - datetime.timedelta(days=1)).strftime("%Y-%m-%d")
+    end_date = now.strftime("%Y-%m-%d")
+
+    params = {"startDate": start_date, "endDate": end_date, "tags": ["neo4j"]}
+
+    sns.publish(TopicArn= topic_arn, Message= json.dumps(params))
 
 
-def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
+def consumer(event, context):
+    for record in event["Records"]:
+        message = json.loads(record["Sns"]["Message"])
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
+        start_date = message["startDate"]
+        end_date = message["endDate"]
+        tags = message["tags"]
 
-    return response
-
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
+        print("start_date: " + start_date)
+        print("end_date: " + end_date)
+        print("tags: " + str(tags))
